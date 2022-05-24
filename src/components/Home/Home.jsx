b80b3753e49e61cfbe544012932bb083c8365077
Augusto Soares <augustocss@gmail.com>
2022-05-24 14:26:19 -0300

    testando useRef hook na home. ainda sem sucesso

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Loading from '../Loading/Loading';
-import { TeamsInfo } from '../../Data.js';
+//import { TeamsInfo } from '../../Data.js';
 
-const moment = require('moment');
 const API = "https://ergast.com/api/f1";
 
 const Home = () => {
-    const [lastRace, setLastRace] = useState([]);
-    const [upcomingRace, setUpcomingRace] = useState([]);
+    //const [lastRace, setLastRace] = useState([]);
+    //const [upcomingRace, setUpcomingRace] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const last = useRef();
+    const next = useRef();
 
     useEffect(() => {
         const loadData = async () => {
@@ -27,8 +28,6 @@ const Home = () => {
             // capturando o round atual (e incrementando 1) numa var temp pois o state abaixo nao fica imediatamente disponivel
             const nextRound = Number(resLastRace.MRData.RaceTable.Races[0].round) + 1;
 
-            setLastRace(resLastRace.MRData.RaceTable.Races);
-
             // load upcoming race
             const resUpcomingRace = await fetch(API + "/2022/" + nextRound + ".json")
                 .then((res) => res.json())
@@ -39,12 +38,16 @@ const Home = () => {
                 });
 
             setLoading(false);
-
-            setUpcomingRace(resUpcomingRace.MRData.RaceTable.Races);
+            //setLastRace(resLastRace.MRData.RaceTable.Races);
+            last.current = resLastRace.MRData.RaceTable.Races[0].Results;
+            //setUpcomingRace(resUpcomingRace.MRData.RaceTable.Races);
+            next.current = resUpcomingRace.MRData.RaceTable.Races[0];
         }
 
         loadData();
     }, [])
+
+    //console.log(last.current);
 
     if (loading) {
         return (
@@ -52,15 +55,18 @@ const Home = () => {
         )
     }
 
-    if (error) {
-        return (
-            <div>Falha no carregamento</div>
-        )
-    }
-
-
     return (
-        <div>Home</div>
+        <div className='homeTable'>
+            <div className='homeRaces'>
+                <h2>Last Round</h2>
+                {(last.current).map((resu) => (
+                    <div>{ resu.number }</div>
+                ))}
+            </div>
+            <div className='homeRaces'>
+
+            </div>
+        </div>
     )
 }
 
